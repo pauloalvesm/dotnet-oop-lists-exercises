1b1e349ab1cb572e87d8ad1b73411f7d8d636bd9
pauloalvesm <paulo.alves7351@gmail.com>
2025-09-21 16:12:02 -0300

    feat: implement solution exercise two

---
+++ src/OOP.Lists.Exercise02/Models/Purchase.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP.Lists.Exercise02.Models;
+
+public class Purchase
+{
+    public DateTime Date { get; set; }
+    public string Product { get; set; }
+    public double Value { get; set; }
+
+    public Purchase(DateTime date, string product, double value)
+    {
+        Date = date;
+        Product = product;
+        Value = value;
+    }
+}
---
+++ src/OOP.Lists.Exercise02/Program.cs
@@ -0,0 +1,49 @@
+﻿using OOP.Lists.Exercise02.Business;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** Purchase Log *****");
+
+        PurchaseLog log = new PurchaseLog();
+
+        Console.WriteLine("Enter the purchase details: ");
+
+        while (true)
+        {
+            Console.Write("Purchase date (YYYY-MM-DD): ");
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime date))
+            {
+                Console.Write("Product name: ");
+                string product = Console.ReadLine();
+
+                Console.Write("Purchase value: ");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    log.AddPurchase(date, product, value);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid value. Make sure to enter a valid number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid date. Make sure to enter a date in YYYY-MM-DD format.");
+            }
+
+            Console.Write("Do you want to add another purchase? (Y/N): ");
+            string answer = Console.ReadLine();
+
+            if (answer.Equals("N", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+        }
+
+        log.ListPurchases();
+
+        Console.ReadKey();
+    }
+}
