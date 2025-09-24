413672045b95b93b81d467b62685ac0c73381023
pauloalvesm <paulo.alves7351@gmail.com>
2025-09-24 19:47:22 -0300

    feat: define solution exercise five

---
+++ src/OOP.Lists.Exercise05/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP.Lists.Exercise05.Models;
+
+public class Job
+{
+    public string Description { get; private set; }
+    public DateTime DueDate { get; private set; }
+
+    public Job(string description, DateTime dueDate)
+    {
+        Description = description;
+        DueDate = dueDate;
+    }
+
+    public bool ShouldBeExecutedToday()
+    {
+        return DueDate.Date == DateTime.Today;
+    }
+}
