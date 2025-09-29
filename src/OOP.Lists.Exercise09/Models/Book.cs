096595cddaedf929237a58aa743615d0e65c4bb0
pauloalvesm <paulo.alves7351@gmail.com>
2025-09-29 20:20:25 -0300

    feat: implement solution exercise nine

---
+++ src/OOP.Lists.Exercise09/Models/Book.cs
@@ -0,0 +1,38 @@
+﻿namespace OOP.Lists.Exercise09.Models;
+
+public class Book
+{
+    private string _title;
+    private string _author;
+
+    public Book(string title, string author)
+    {
+        ValidateData(title, author);
+        _title = title;
+        _author = author;
+    }
+
+    public string Title
+    {
+        get { return _title; }
+        set { _title = value; }
+    }
+
+    public string Author
+    {
+        get { return _author; }
+        set { _author = value; }
+    }
+
+    private void ValidateData(string title, string name)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            throw new ArgumentException("The book title cannot be empty or null!");
+        }
+        else if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("The author name cannot be empty or null!");
+        }
+    }
+}
