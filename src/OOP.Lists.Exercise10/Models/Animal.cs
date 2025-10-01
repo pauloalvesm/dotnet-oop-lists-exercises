0da9d5bfe18550b8a9167fdd3b11792128e06683
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-01 20:24:24 -0300

    feat: define solution exercise ten

---
+++ src/OOP.Lists.Exercise10/Models/Animal.cs
@@ -0,0 +1,38 @@
+﻿namespace OOP.Lists.Exercise10.Models;
+
+public class Animal
+{
+    protected string _name;
+    protected string _species;
+    protected int _age;
+
+    public Animal(string name, string species, int age)
+    {
+        _name = name;
+        _species = species;
+        _age = age;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public string Species
+    {
+        get { return _species; }
+        set { _species = value; }
+    }
+
+    public int Age 
+    {
+        get { return _age; }
+        set { _age = value; }
+    }
+
+    public virtual void MakeSound(string animalSound)
+    {
+        Console.WriteLine(animalSound);
+    }
+}
---
+++ src/OOP.Lists.Exercise10/Program.cs
@@ -0,0 +1,41 @@
+﻿using OOP.Lists.Exercise10.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** Inheritance Demonstration (Dog and Cat) *****\n");
+
+        Console.WriteLine("--- Creating a Dog ---");
+        Console.Write("Dog's Name: ");
+        string dogName = Console.ReadLine();
+        Console.Write("Dog's Age: ");
+        int dogAge = int.Parse(Console.ReadLine());
+
+        Dog myDog = new Dog(dogName, dogAge);
+
+        Console.WriteLine("\n----- Dog Result -----");
+        Console.WriteLine($"Name: {myDog.Name}");
+        Console.WriteLine($"Species: {myDog.Species}");
+        Console.WriteLine($"Age: {myDog.Age} years");
+        myDog.MakeSound();
+
+        Console.WriteLine("\n----------------------------------------------------");
+
+        Console.WriteLine("\n--- Creating a Cat ---");
+        Console.Write("Cat's Name: ");
+        string catName = Console.ReadLine();
+        Console.Write("Cat's Age: ");
+        int catAge = int.Parse(Console.ReadLine());
+
+        Cat myCat = new Cat(catName, catAge);
+
+        Console.WriteLine("\n----- Cat Result -----");
+        Console.WriteLine($"Name: {myCat.Name}");
+        Console.WriteLine($"Species: {myCat.Species}");
+        Console.WriteLine($"Age: {myCat.Age} years");
+        myCat.MakeSound();
+
+        Console.ReadKey();
+    }
+}
