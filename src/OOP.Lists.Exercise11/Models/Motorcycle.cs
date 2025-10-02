182a73388ae7934ec97f938e75dbdd4a5f4a71b2
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-02 20:42:20 -0300

    feat: implement solution exercise eleven

---
+++ src/OOP.Lists.Exercise11/Models/Motorcycle.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP.Lists.Exercise11.Models;
+
+public class Motorcycle : Vehicle
+{
+    public Motorcycle(string model, int year) : base(model, year) {}
+
+    public override void Accelerate(int increment)
+    {
+        UpdateSpeed(Speed + increment);
+        Console.WriteLine($"Motorcycle ({Model}) accelerates by {increment} km/h. New Speed: {Speed} km/h.");
+    }
+
+    public override void Brake(int decrement)
+    {
+        UpdateSpeed(Speed - decrement);
+        Console.WriteLine($"Motorcycle ({Model}) brakes by {decrement} km/h. New Speed: {Speed} km/h.");
+    }
+
+    public override string ToString()
+    {
+        return $"[Motorcycle] Model: {Model}, Year: {Year}, Current Speed: {Speed} km/h";
+    }
+}
---
+++ src/OOP.Lists.Exercise11/Models/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP.Lists.Exercise11.Models;
+
+public abstract class Vehicle
+{
+    public string Model { get; }
+    public int Year { get; }
+    public int Speed { get; private set; }
+
+    public Vehicle(string model, int year, int initialSpeed = 0)
+    {
+        Model = model;
+        Year = year;
+        Speed = initialSpeed;
+    }
+
+    public abstract void Accelerate(int increment);
+    public abstract void Brake(int decrement);
+
+    protected void UpdateSpeed(int newSpeed)
+    {
+        Speed = Math.Max(0, newSpeed);
+    }
+
+    public override string ToString()
+    {
+        return $"[Vehicle] Model: {Model}, Year: {Year}, Current Speed: {Speed} km/h";
+    }
+}
