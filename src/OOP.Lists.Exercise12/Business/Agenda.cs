413dee45d9006253bf10997590ed062460c25c37
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-03 21:21:35 -0300

    feat: apply solution exercise twelve

---
+++ src/OOP.Lists.Exercise12/Business/Agenda.cs
@@ -0,0 +1,48 @@
+﻿using OOP.Lists.Exercise12.Models;
+
+namespace OOP.Lists.Exercise12.Business;
+
+public class Agenda
+{
+    private List<Person> _personList;
+
+    public Agenda()
+    {
+        _personList = new List<Person>();
+    }
+
+    public void StorePerson(string name, int age, float height)
+    {
+        Person person = new Person(name, age, height);
+        _personList.Add(person);
+    }
+
+    public void RemovePerson(string name)
+    {
+        Person person = SearchPerson(name);
+
+        if (person != null)
+        {
+            _personList.Remove(person);
+        }
+        else
+        {
+            Console.WriteLine("Person not found in the agenda.");
+        }
+    }
+
+    public Person SearchPerson(string name)
+    {
+        return _personList.Find(p => p.Name == name);
+    }
+
+    public void PrintAgenda()
+    {
+        Console.WriteLine("Agenda:");
+
+        foreach (var person in _personList)
+        {
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
---
+++ src/OOP.Lists.Exercise12/Program.cs
@@ -0,0 +1,88 @@
+﻿using OOP.Lists.Exercise12.Business;
+using OOP.Lists.Exercise12.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** Contact Agenda *****");
+
+        Agenda agenda = new Agenda();
+
+        while (true)
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Add Person");
+            Console.WriteLine("2. Remove Person");
+            Console.WriteLine("3. Search Person");
+            Console.WriteLine("4. Print Agenda");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("\nOption: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int option))
+            {
+                Console.WriteLine("Invalid option, please try again!");
+                continue;
+            }
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("\nEnter the person's data: ");
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Age: ");
+                    if (!int.TryParse(Console.ReadLine(), out int age))
+                    {
+                        Console.WriteLine("Invalid Age format. Returning to main menu.");
+                        break;
+                    }
+
+                    Console.Write("Height: ");
+                    if (!float.TryParse(Console.ReadLine(), System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float height))
+                    {
+                        Console.WriteLine("Invalid Height format. Returning to main menu.");
+                        break;
+                    }
+
+                    agenda.StorePerson(name, age, height);
+                    break;
+
+                case 2:
+                    Console.Write("Name of the person to be removed: ");
+                    string nameForRemoval = Console.ReadLine();
+                    agenda.RemovePerson(nameForRemoval);
+                    break;
+
+                case 3:
+                    Console.Write("Name of the person you want to search: ");
+                    string nameForSearch = Console.ReadLine();
+                    Person person = agenda.SearchPerson(nameForSearch);
+
+                    if (person != null)
+                    {
+                        Console.WriteLine($"Person found - {person}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Person not found in the agenda!");
+                    }
+                    break;
+
+                case 4:
+                    agenda.PrintAgenda();
+                    break;
+
+                case 5:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option, please try again!");
+                    break;
+            }
+        }
+    }
+}
